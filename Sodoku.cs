24baf20ef476494a480b27d388a32c557e8c9fcc
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2017-09-18 20:03:19 +0200

    uppdaterat med indatasträng med svårt soduku. Ingen fungerande lösning för svåra spel finns i koden än

--- Sodoku.cs
+++ Sodoku.cs
@@ -90,7 +90,6 @@ namespace Soduko
                     if (item.PossibleNumbers.Count == 1)
                     {
                         count++;
-                        Console.WriteLine(count);
                     }
 
                 }
--- SodokuGame.cs
+++ SodokuGame.cs
@@ -10,9 +10,15 @@ namespace Soduko
     {
         static void Main(string[] args)
         {
-            Sodoku game = new Sodoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+            //"lätt" Soduku. svårighetsgrad som kan lösas helt endast med hjälp av sodukureglerna 
+            //Sodoku game = new Sodoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+
+            // "svårt". Man kommer inte fram till helt utan måste köra någon sorts gissningsvariant som inte finns i programmet än
+            Sodoku game = new Sodoku("037060000205000800006908000" +
+                                     "000600024001503600650009000" +
+                                     "000302700009000402000050360");
+
             
-            //Sodoku game = new Sodoku("003020600900000001001806400008102900700000008006708200002009500800203009005010300");
             game.Solve();
 
             game.BoardAsText();
