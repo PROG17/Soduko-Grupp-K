11224c2fcf500d2346bf1fb2382ad0091488a0a4
johandolph <31857900+johandolph@users.noreply.github.com>
2017-09-20 17:23:28 +0200

    Added a new Class

---
+++ SudokuBoard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soduko
+{
+    class SudokuBoard
+    {
+        private int row;
+        private int col;
+        public char[] textArray;
+
+        public void TextToArray()
+        {
+            string text = "037060000205000800006908000000600024001503600650009000000302700009000402000050360";
+            textArray = text.ToCharArray();
+        }
+
+
+        //private int[,] numbers = new int[9, 9];
+
+
+
+
+
+        public void checkNumber(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        private void checkRow()
+        {
+
+        }
+
+        private void checkCol()
+        {
+
+        }
+
+        private void checkBox()
+        {
+
+        }
+    }
+}
